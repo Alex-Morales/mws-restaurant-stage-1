f0b1b4ab4baf3bf83597af6bdefb9f132a773b92
Alex Morales <a.jrmorales@gmail.com>
2018-09-03 19:54:03 -0700

    Service worker is set up and is caching data.

---
+++ sw.js
@@ -0,0 +1,57 @@
+var cacheID = "mws-restaurant-10";
+
+const cacheFiles = [
+  "/",
+  "/index.html",
+  "/restaurant.html",
+  "/css/styles.css",
+  "/data/restaurants.json",
+  "/js/dbhelper.js",
+  "/js/main.js",
+  "/js/restaurant_info.js",
+  "/img/1.jpg",
+  "/img/2.jpg",
+  "/img/3.jpg",
+  "/img/4.jpg",
+  "/img/5.jpg",
+  "/img/6.jpg",
+  "/img/7.jpg",
+  "/img/8.jpg",
+  "/img/9.jpg",
+  "/img/10.jpg"
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheID).then(cache => {
+      console.log("Caches open successful!");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if(response) {
+        console.log("Found ", event.request, "in cache.");
+        return response;
+      }
+      else {
+        console.log("Could not find ", event.request, "in cache, FETCHING!");
+        return fetch(event.request)
+        .then(response => {
+          const clonedResponse = response.clone();
+          caches.open(cacheID).then(cache => {
+            cache.put(event.request, clonedResponse);
+          })
+          return response;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
+    })
+  );
+});
