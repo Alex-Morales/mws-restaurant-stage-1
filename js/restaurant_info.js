2772e230bbd95cb5ed3647f08b700cdc84c0a41a
Alex Morales <a.jrmorales@gmail.com>
2018-09-04 15:26:29 -0700

    misspelled restaurant in last commit.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,7 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `Image of ${restaurant.name} restaurant in ${resturant.neighborhood}`;
+  image.alt = `Image of ${restaurant.name} restaurant in ${restaurant.neighborhood}`;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
